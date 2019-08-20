799a336019dfe5e96c29cda65f88fca09264f871
Francisco Carrasco <xnofub@gmail.com>
2019-08-19 21:48:43 -0400

    LOGIN
    LOGIN

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,7 +44,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                    {{ __('Iniciar Sesión') }}
                                 </button>
 
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
