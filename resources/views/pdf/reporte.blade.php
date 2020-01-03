f3017f99076f27adac46838a7f26593f59114d6e
Ricardo Parra molina <rparra@MacBook-Pro-de-Ricardo.local>
2020-01-02 21:32:25 -0300

    dfsgsdfg

--- resources/views/pdf/reporte.blade.php
+++ resources/views/pdf/reporte.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 <div class="row">
-    <div class="logo" align="center">
+    <div class="logo" align="center" style="width: 10%;height: 10%;">
         <div>
             <img src="logo_png.png">
 
