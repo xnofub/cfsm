22778ebe9a5ad6e90b8e8d480375a5d991cbbc65
Ricardo Parra <ricardoparramolina@gmail.com>
2020-12-22 22:44:17 -0300

    feat: Add enpoint for update app values

---
+++ app/Http/Controllers/API/MobileApiController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Services\MobileService;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MobileApiController extends Controller
+{
+    //
+    public function sync(MobileService $service, Request $request)
+    {
+        $response = $service->sync($request);
+        if ($response) {
+            return response()->json(['data' =>  $response], 200);
+        }
+        return response()->json(['data' => ['message' => 'Wrong request format']], 500);
+
+    }
+}
