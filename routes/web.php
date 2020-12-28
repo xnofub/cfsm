30abe0d4605157ab7edfc9bece9b4a2b71556ddd
Ricardo Parra <ricardoparramolina@gmail.com>
2020-12-28 00:24:29 -0300

    fix: Change route

--- routes/web.php
+++ routes/web.php
@@ -163,7 +163,7 @@ Route::group(['middleware' => ['web']], function () { #auth
     Route::get('productorVariedadDatetables', 'ProductorVariedadController@productorVariedadDatetables');
 
 
-    Route::post('getDataByProductoresId', 'AdminController@muestrasDatetables');
+    Route::post('getDataByProductoresId', 'AdminController@getDataByProductoresId');
 
     Route::post('vergraficos', 'GraficoController@vergraficos')->name('vergraficos');
     Route::post('reporteConsolidado', 'MuestraController@GetReporteConsolidado')->name('reporteConsolidado');
