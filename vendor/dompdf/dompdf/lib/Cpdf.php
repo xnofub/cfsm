27698d4f5b561db8802ee9dc710cbfb7b2608cfb
Francisco Carrasco <xnofub@gmail.com>
2021-01-21 17:02:09 -0300

    FIX EXCEL
    FIX EXCEL https://github.com/dompdf/dompdf/pull/2027/files

--- vendor/dompdf/dompdf/lib/Cpdf.php
+++ vendor/dompdf/dompdf/lib/Cpdf.php
@@ -2542,7 +2542,8 @@ EOT;
                             $width = floatval($dtmp['WX']);
 
                             if ($c >= 0) {
-                                if ($c != hexdec($n)) {
+                                //if ($c != hexdec($n)) {
+                                if (ctype_xdigit($c) && ctype_xdigit($n)) {
                                     $data['codeToName'][$c] = $n;
                                 }
                                 $data['C'][$c] = $width;
@@ -2595,7 +2596,8 @@ EOT;
                                     $cidtogid[$c * 2 + 1] = chr($glyph & 0xFF);
                                 }
 
-                                if ($c != hexdec($n)) {
+                                //if ($c != hexdec($n)) {
+                                if (ctype_xdigit($c) && ctype_xdigit($n)) {
                                     $data['codeToName'][$c] = $n;
                                 }
                                 $data['C'][$c] = $width;
